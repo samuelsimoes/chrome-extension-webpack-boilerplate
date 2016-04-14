cda77e14ffb189161a446dd9a9561c11a8403913
Samuel Simões <samuel@samuelsimoes.com>
2016-04-14 20:15:54 -0300

    Adding support to React (with HRM)

---
+++ src/js/popup/greeting_component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import icon from "../../img/icon-128.png"
+
+export default class extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>Hello, find me on src/js/popup/greeting_component.jsx</p>
+        <img src={icon} />
+      </div>
+    )
+  }
+};
