77926fe87f87f17223d1456e19885098536e5541
Roman Vasilev <2rvasilev@live.ru>
2017-11-25 21:23:28 +0200

    Add ability to configure webpack-dev-server from main webpack.config.js
    As example - colorize console output

--- utils/webserver.js
+++ utils/webserver.js
@@ -25,7 +25,7 @@ delete config.chromeExtensionBoilerplate;
 var compiler = webpack(config);
 
 var server =
-  new WebpackDevServer(compiler, {
+  new WebpackDevServer(compiler, config.devServer, {
     hot: true,
     contentBase: path.join(__dirname, "../build"),
     headers: { "Access-Control-Allow-Origin": "*" }
