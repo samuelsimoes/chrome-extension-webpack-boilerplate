0c4d5382422824210783021960249a3f1615c685
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-01-25 00:31:16 -0800

    Use the webpack-chrome-extension-reloader plugin to update the chrome extension. This fixes issues such as the react lifecycle not working properly, or issues with locals when building webpack

--- utils/webserver.js
+++ utils/webserver.js
@@ -1,24 +1,13 @@
 var WebpackDevServer = require("webpack-dev-server"),
+    ChromeExtensionReloader  = require('webpack-chrome-extension-reloader'),
     webpack = require("webpack"),
     config = require("../webpack.config"),
     env = require("./env"),
     path = require("path");
 
 var options = (config.chromeExtensionBoilerplate || {});
-var excludeEntriesToHotReload = (options.notHotReload || []);
-
-for (var entryName in config.entry) {
-  if (excludeEntriesToHotReload.indexOf(entryName) === -1) {
-    config.entry[entryName] =
-      [
-        ("webpack-dev-server/client?http://localhost:" + env.PORT),
-        "webpack/hot/dev-server"
-      ].concat(config.entry[entryName]);
-  }
-}
-
 config.plugins =
-  [new webpack.HotModuleReplacementPlugin()].concat(config.plugins || []);
+  [new ChromeExtensionReloader()].concat(config.plugins || []);
 
 delete config.chromeExtensionBoilerplate;
 
@@ -26,7 +15,6 @@ var compiler = webpack(config);
 
 var server =
   new WebpackDevServer(compiler, {
-    hot: true,
     contentBase: path.join(__dirname, "../build"),
     headers: { "Access-Control-Allow-Origin": "*" }
   });
