5a142cb07ff862b9b14f4b05960a3582196d3486
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-02-06 15:41:31 -0600

    Finished Audio Integration

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -1,36 +1,68 @@
 import React from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
 import ImageView from './imageView.jsx';
+import secrets from './secrets.js';
+import axios from 'axios';
 
 const unsplash = new Unsplash({
-  applicationId: 'cddfbdb3b08a9e68a8766d6ab7c07893a3ef6571020af994bf21b5631c6be802',
-  secret: '234b6003eac47b8ac815c977e157d96b72d23b687723f5d2af4dd07430c4c721',
-  callbackUrl: 'urn:ietf:wg:oauth:2.0:oob'
+  applicationId: secrets.UNSPLASH_APP_ID,
+  secret: secrets.UNSPLASH_SECRET,
+  callbackUrl: secrets.UNSPLASH_CALLBACK
 });
 
-let renderCount = 0;
 export default class Main extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       wordImages: [],
+      word: "dog",
+      definition: [],
+      audioData: []
     }
   }
   componentDidMount() {
+    let wordnikStart = "http://api.wordnik.com:80/v4/word.json";
+    let definitionEnd = `definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=${secrets.WORDNIK_API_KEY}`;
+    let audioEnd = `audio?useCanonical=false&limit=50&api_key=${secrets.WORDNIK_API_KEY}`
+    let word = this.state.word;
+
+    // Request for Photographs
     unsplash.search.photos('dogs', 1, 4)
       .then(toJson)
       .then(json => {
         this.setState({ wordImages: json });
       });
+
+    // Request for Definition
+    axios.get(`${wordnikStart}/${word}/${definitionEnd}`)
+      .then(toJson)
+      .then(item => {
+        console.log('wordData', item.data);
+        this.setState({ definition: item.data });
+      });
+
+    // Request for audio
+    axios.get(`${wordnikStart}/${word}/${audioEnd}`)
+      .then(toJson)
+      .then(item => {
+        console.log('audio', item.data);
+        this.setState({ audioData: item.data });
+      });
   }
   render() {
+    console.log(this.state);
     let imageArr = this.state.wordImages.results;
+    let wordData = this.state.definition;
+    let audioData = this.state.audioData;
+    if (!imageArr || !wordData.length || !audioData.length) return (<div />);
     return (
       <div>
         {imageArr.map(image => <ImageView imageURL={image.urls.small} />)}
-        <h2>Dogs</h2>
-        <p>a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, and a barking, howling, or whining voice. It is widely kept as a pet or for work or field sports.</p>
-        <button>Audio</button>
+        <h2>{this.state.word}</h2>
+        <p>{wordData[0].text}</p>
+        <audio controls>
+          <source src={this.state.audioData[0].fileUrl} />
+        </audio>
       </div>
     );
   }
