655a5dd25d80fe0f7fe9f698ae4903e7b20a0488
WhiteMind <WhiteMind@qq.com>
2019-11-24 02:17:19 +0800

    Removing meaningless escape

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ var options = {
         exclude: /node_modules/
       },
       {
-        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
         loader: "file-loader?name=[name].[ext]",
         exclude: /node_modules/
       },
