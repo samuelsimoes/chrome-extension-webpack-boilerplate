6d103fae4dee6ce503f2173ada222bf85d051484
Igor Pellegrini <igor.pellegrini@live.com>
2020-11-14 02:22:06 +0100

    Support hot-reload of the extension, at build
    1) First run a build of the app:
      yarn build
    
    2) Then enable `Developer mode` in Chrome Extensions and `Load unpacked`
    from the `./build` directory just created at point 1)
    
    If you break the extension on the browser and it reloads wrongly you
    might need to redo the previous steps for restoring a correctly running
    hot-reload script.
    --
    
    TIP:
    You can trigger a build command when saving files, if you use SublimeText,
    just installing `SublimeOnSaveBuild` via Package Control,
    and adding the config in Tool->BuildSystem->NewBuildSystem:
    
    {
        "working_dir": "/path/to//SearchSynonyms",
        "shell_cmd": "cd $folder && yarn build"
    }
    ($folder is a Sublime special var)
    
    This allows you to do SaveFile->RebuildExtension->ReloadExtension
    You might need to reload the current browser tabs, in otder to see
    the new changes.

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ var options = {
   entry: {
     popup: path.join(__dirname, "src", "js", "popup.js"),
     options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    background: path.join(__dirname, "src", "js", "background.js"),
+    hot_reload: path.join(__dirname, "src", "js", "hot-reload.js")
   },
   output: {
     path: path.join(__dirname, "build"),
