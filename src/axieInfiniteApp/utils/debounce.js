b0e0b2af8063d1497bb5013d3b6233d1b91a502c
Daniel Padrino <daniel.padrino@inferstat.com>
2021-07-19 13:52:15 -0300

    include debounce implementation

---
+++ src/axieInfiniteApp/utils/debounce.js
@@ -0,0 +1,15 @@
+const debounce = (func, wait) => {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+export default debounce;
